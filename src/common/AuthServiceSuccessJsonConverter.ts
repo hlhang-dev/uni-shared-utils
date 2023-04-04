bb35f2984ce4cf6fe47d436411ee4196f00f4ceb
hlhang-dev <hlhang@outlook.com>
2023-04-04 16:10:17 +0800

    🚀: reset current branch

--- src/common/AuthServiceSuccessJsonConverter.ts
+++ src/common/AuthServiceSuccessJsonConverter.ts
@@ -1,21 +1,14 @@
 import { JsonConverter, JsonCustomConvert } from 'json2typescript'
-import { Utils } from 'ts-dev-common-utils'
 
 @JsonConverter
 export default class AuthServiceSuccessJsonConverter implements JsonCustomConvert<boolean> {
-    public static successCode: number | boolean = 1
+    public static successCode = 1
 
     serialize(data: boolean): any {
         return null
     }
 
     deserialize(data: any): boolean {
-        let isSuccess: boolean
-        if (Utils.isBoolean(AuthServiceSuccessJsonConverter.successCode)) {
-            isSuccess = (data === AuthServiceSuccessJsonConverter.successCode)
-        }else {
-            isSuccess = (Number(data) === AuthServiceSuccessJsonConverter.successCode)
-        }
-        return isSuccess
+        return Number(data) === AuthServiceSuccessJsonConverter.successCode
     }
 }
